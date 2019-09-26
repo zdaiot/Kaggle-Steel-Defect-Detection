3617faac65a25fba8e0d580f89f9e556bdde4114
XiangqianMa <xiangqianma@outlook.com>
2019-09-26 12:20:27 +0800

    fix model re-init bug

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ import pickle
 import random
 
 class TrainVal():
-    def __init__(self, config):
+    def __init__(self, config, fold):
         # 加载网络模型
         self.model_name = config.model_name
         self.model = Model(self.model_name).create_model()
@@ -25,6 +25,7 @@ class TrainVal():
         self.lr = config.lr
         self.weight_decay = config.weight_decay
         self.epoch = config.epoch
+        self.fold = fold
 
         # 实例化实现各种子函数的 solver 类
         self.solver = solver(self.model)
@@ -38,7 +39,7 @@ class TrainVal():
             os.makedirs(self.model_path)
 
         # 保存json文件和初始化tensorboard
-        TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.datetime.now())
+        TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S-%d}".format(datetime.datetime.now(), fold)
         self.writer = SummaryWriter(log_dir=os.path.join(self.model_path, TIMESTAMP))
         with codecs.open(self.model_path + '/'+ TIMESTAMP + '.json', 'w', "utf-8") as json_file:
             json.dump({k: v for k, v in config._get_kwargs()}, json_file, ensure_ascii=False)
@@ -51,7 +52,7 @@ class TrainVal():
         with open(self.model_path + '/'+ TIMESTAMP + '.pkl','wb') as f:
             pickle.dump({'seed': self.seed}, f, -1)
 
-    def train(self, train_loader, valid_loader, fold):
+    def train(self, train_loader, valid_loader):
         ''' 完成模型的训练，保存模型与日志
         Args:
             train_loader: 训练数据的DataLoader
@@ -78,7 +79,7 @@ class TrainVal():
                 # 保存到tensorboard，每一步存储一个
                 self.writer.add_scalar('train_loss', loss.item(), global_step+i)
 
-                descript = "Fold: %d, Train Loss: %.7f, lr: %s" % (fold, loss.item(), self.lr)
+                descript = "Fold: %d, Train Loss: %.7f, lr: %s" % (self.fold, loss.item(), self.lr)
                 tbar.set_description(desc=descript)
 
             # 每一个epoch完毕之后，执行学习率衰减
@@ -101,7 +102,7 @@ class TrainVal():
                 'max_dice_valid': self.max_dice_valid,
             }
 
-            self.solver.save_checkpoint(os.path.join(self.model_path, '%s_fold%d.pth' % (self.model_name, fold)), state, is_best)
+            self.solver.save_checkpoint(os.path.join(self.model_path, '%s_fold%d.pth' % (self.model_name, self.fold)), state, is_best)
             self.writer.add_scalar('valid_loss', loss_valid, epoch)
             self.writer.add_scalar('valid_dice', dice_valid, epoch)
 
@@ -144,6 +145,8 @@ if __name__ == "__main__":
     mean=(0.485, 0.456, 0.406)
     std=(0.229, 0.224, 0.225)
     dataloaders = provider(config.dataset_root, os.path.join(config.dataset_root, 'train.csv'), mean, std, config.batch_size, config.num_workers, config.n_splits)
-    train_val = TrainVal(config)
     for fold_index, [train_loader, valid_loader] in enumerate(dataloaders):
-        train_val.train(train_loader, valid_loader, fold_index)
+        if fold_index == 0:
+            continue
+        train_val = TrainVal(config, fold_index)
+        train_val.train(train_loader, valid_loader)
